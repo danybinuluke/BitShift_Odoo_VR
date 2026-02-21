b83c50e143c3623141ac7d1f697985880d23e069
mehul-77 <mehulmadhu04@gmail.com>
2026-02-21 10:43:47 +0530

    feat: Implement initial backend API for managing vehicles, drivers, trips, and maintenance using Express and Prisma.

---
+++ backend/src/controllers/driver.controller.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const createDriver = async (req, res) => {
+    const driver = await prisma.driver.create({
+        data: req.body
+    });
+    res.json(driver);
+};
+
+export const getDrivers = async (req, res) => {
+    const drivers = await prisma.driver.findMany();
+    res.json(drivers);
+};
