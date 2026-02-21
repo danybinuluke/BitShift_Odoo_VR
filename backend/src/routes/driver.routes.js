a99f03cb7be6fee78da2006a97029c92fd14ad6e
mehul-77 <mehulmadhu04@gmail.com>
2026-02-21 13:30:35 +0530

    New updation for RBAC1

--- backend/src/routes/driver.routes.js
+++ backend/src/routes/driver.routes.js
@@ -1,9 +1,17 @@
 import express from "express";
+import {
+    createDriver,
+    getDrivers
+} from "../controllers/driver.controller.js";
+
+import { protect, authorize } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.json({ message: "Driver route working" });
-});
+/* MANAGER or SAFETY can add driver */
+router.post("/", protect, authorize("MANAGER", "SAFETY"), createDriver);
+
+/* Logged in users can view drivers */
+router.get("/", protect, getDrivers);
 
 export default router;
